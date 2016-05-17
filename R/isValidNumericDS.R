4156a20c4d08315a454b6f7ca060518cc4228171
davraam <avraam_dimitris@hotmail.com>
2016-05-17 15:29:54 +0100

    Update isValidNumericDS.R

--- R/isValidNumericDS.R
+++ R/isValidNumericDS.R
@@ -1,27 +1,29 @@
 #'
-#' @title Computes 
-#' @description jhvjh
-#' @details sffgg
-#' @param obj
-#' @return a boolean
-#' @author fgg
+#' @title Checks if an input variable is valid
+#' @description Tells if an object on the server side is valid.
+#' @details This function checks if an object is valid. The object is considered as valid if has more
+#' than two unique values (excluding missing values) or if has only two unique values but the counts
+#' of each unique category is more than the protection filter set for table cell counts. 
+#' @param obj, a vector of a variable
+#' @return a binary value, 0 if input valid is valid or 1 if not.
+#' @author Burton, P., Avraam, D.,
 #' @export
 #'
 isValidNumericDS <- function(obj){
 
   unique.values.noNA <- unique(obj[complete.cases(obj)])
-  toosmall <- 0
+  valid <- 0
 
   if(length(unique.values.noNA)==2){
     tabvar <- table(obj)[table(obj)>=1]
     min.category <- min(tabvar)
     nf.tab <- getOption("nfilter.tab")
     if(min.category < nf.tab){
-      toosmall <- 1
+      valid <- 1
     }
   }
  
-return(toosmall)
+return(valid)
 
 }
 
